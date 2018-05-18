901c2297579f155add0e2cc3e183445f2e1b14d6
Matthias Möller <m_moeller@live.de>
2018-05-18 15:03:51 +0200

    add example test & cleanup example files

--- python_example_script.py
+++ python_example_script.py
@@ -1,35 +1,37 @@
+print("Hello")
+
 # from OpenMesh Tutorial
 # http://openmesh.org/Daily-Builds/Doc/a03957.html
 from openmesh import *
 
 mesh = TriMesh()
 
-vh0 = mesh.add_vertex(TriMesh.Point(0, 1, 0))
-vh1 = mesh.add_vertex(TriMesh.Point(1, 0, 0))
-vh2 = mesh.add_vertex(TriMesh.Point(2, 1, 0))
-vh3 = mesh.add_vertex(TriMesh.Point(0,-1, 0))
-vh4 = mesh.add_vertex(TriMesh.Point(2,-1, 0))
+vh0 = mesh.add_vertex([0, 1, 0])
+vh1 = mesh.add_vertex([1, 0, 0])
+vh2 = mesh.add_vertex([2, 1, 0])
+vh3 = mesh.add_vertex([0,-1, 0])
+vh4 = mesh.add_vertex([2,-1, 0])
+print("Points added")
 
 fh0 = mesh.add_face(vh0, vh1, vh2)
 fh1 = mesh.add_face(vh1, vh3, vh4)
 fh2 = mesh.add_face(vh0, vh3, vh1)
+print("faces added")
 
 vh_list = [vh2, vh1, vh4]
 fh3 = mesh.add_face(vh_list)
 
-prop_handle = VPropHandle()
-mesh.add_property(prop_handle, "cogs")
+#prop_handle = VPropHandle()
+mesh.vertex_property("intProp")
 
 for vh in mesh.vertices():
-    cog = TriMesh.Point(0,0,0)
-    valence = 0
-    for neighbor in mesh.vv(vh):
-        cog += mesh.point(neighbor)
-        valence += 1
-    mesh.set_property(prop_handle, vh, cog / valence)
+    mesh.set_vertex_property("intProp", vh, vh.idx())
 
 # !!! Don't Forget: normals are not updated automatically !!!
 mesh.update_normals()
+
+mesh_id = openflipper.get_id(mesh)
+print("Mesh ID: {}".format(mesh_id))
 
 # build-in openflipper module
 for name, mesh in openflipper.meshes().items():
@@ -37,6 +39,7 @@ for name, mesh in openflipper.meshes().items():
 	
 # experimental function calls over RPC
 cube_id = openflipper.rpc_call("primitivesgenerator","addCube")
+openflipper.rpc_call("backup","createBackup",["int",cube_id, "QString","testing_backup","UpdateType", openflipper.Update.GEOMETRY | openflipper.Update.TOPOLOGY])
 cube_mesh = openflipper.get_mesh(cube_id)
 openflipper.rpc_call("core","deleteObject",["int",cube_id])
 
